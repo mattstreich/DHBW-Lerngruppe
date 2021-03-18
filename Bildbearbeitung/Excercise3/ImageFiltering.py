ea8f5bafdb9ce9fcc7ffc6132db30869713bd5a2
mattstreich <matthias.streich@atos.net>
2021-03-18 02:04:38 +0100

    Matthias hasst sich selber

--- Bildbearbeitung/Excercise3/ImageFiltering.py
+++ Bildbearbeitung/Excercise3/ImageFiltering.py
@@ -5,42 +5,259 @@ import cv2
 
 # apply median filter
 def applyMedianFilter(img, kSize):
+    #temporaeres Bild zum Speicher der Ergebnisse des Filters
+    tmp = img
+
+    borderSize = int(kSize/2)
+
+    #Bild erweitern, damit Groesse Eingang = Groese Ausgang
+    img = cv2.copyMakeBorder(
+        img,
+        top=borderSize,
+        bottom=borderSize,
+        left=borderSize,
+        right=borderSize,
+        borderType=cv2.BORDER_REFLECT_101
+    )
+
+    #Position des Medians
+    median_pos = int(kSize / 2)
+
+    for i in range(tmp.shape[0]):
+        for j in range(tmp.shape[1]):
+            
+            median = []
+            #Filter fuellen
+
+            for u in range(kSize):
+                for v in range(kSize):
+                    median.append(img[i+u,j+v])
+
+            #calculieren des medians
+            median.sort()
+            #median uertragen
+            tmp[i,j] = median[median_pos]
+
+    #Gefilternes Bild uebertragen
+    img = tmp
+
     return 0
 
 # create a moving average kernel of arbitrary size
-def createMovingAverageKernel(kSize):
-    return 0
+def applyMovingAverageFilter(img, kSize):
 
-def gaussian( x, y, sigmaX, sigmaY, meanX, meanY):
+    #temporaeres Bild zum Speicher der Ergebnisse des Filters
+    tmp = img
+ 
+    borderSize = int(kSize/2)
+
+    #Bild erweitern, damit Groesse Eingang = Groese Ausgang
+    img = cv2.copyMakeBorder(
+        img,
+        top=borderSize,
+        bottom=borderSize,
+        left=borderSize,
+        right=borderSize,
+        borderType=cv2.BORDER_REFLECT_101
+    )
+
+    for i in range(tmp.shape[0]):
+        for j in range(tmp.shape[1]):
+            
+            average = 0 
+
+            for u in range(kSize):
+                for v in range(kSize):
+                    average += img[i+u,j+v]
+
+
+            #median uertragen
+            tmp[i,j] = int(average/(kSize*kSize))
+
+    #Gefilternes Bild uebertragen
+    img = tmp
     return 0
 
 # create a gaussian kernel of arbitrary size
-def createGaussianKernel(kSize):
+def createGaussianKernel(kSize, stdDeviation):
+
+    kernel = np.empty([kSize,kSize])
+
+    for i in range(kSize):
+        for j in range(kSize):
+            kernel[i,j] = ( 1/(np.sqrt(2 * np.pi)) ) * np.exp(-((np.square(i)+np.square(j))/(2*stdDeviation)))
+
+    return kernel
+
+def applyGaussFilter(img, kSize, stdDeviation):
+    #temporaeres Bild zum Speicher der Ergebnisse des Filters
+    tmp = img
+
+    borderSize = int(kSize/2)
+
+    #Bild erweitern, damit Groesse Eingang = Groese Ausgang
+    img = cv2.copyMakeBorder(
+        img,
+        top=borderSize,
+        bottom=borderSize,
+        left=borderSize,
+        right=borderSize,
+        borderType=cv2.BORDER_REFLECT_101
+    )
+
+    gausskernel = createGaussianKernel(kSize,stdDeviation)
+
+    for i in range(tmp.shape[0]):
+        for j in range(tmp.shape[1]):
+
+            gauss = 0
+
+            for u in range(kSize):
+                for v in range(kSize):
+                    gauss += img[i+u,j+v] * gausskernel[u,v]
+
+            tmp[i,j] = int(np.round(gauss))
+
+    #Gefilternes Bild uebertragen
+    img = tmp
+
     return 0
 
 # create a kernel of size 3x3
 def createSobelXKernel():
 
-    return 0
+    kernel = [[1,0,-1],[2,0,-2],[1,0,-1]]
+    kernel = np.array(kernel)
+
+    return kernel
 
 # create a kernel of size 3x3
 def createSobelYKernel():
 
-    return 0
+    kernel = [[1,2,1],[0,0,0],[-1,-2,-1]]
+    kernel = np.array(kernel)
+
+    return kernel
 
 def applyKernelInSpatialDomain(img, kernel):
+    #temporaeres Bild zum Speicher der Ergebnisse des Filters
+    tmp = img
 
+
+    #Bild erweitern, damit Groesse Eingang = Groese Ausgang
+    img = cv2.copyMakeBorder(
+        img,
+        top=1,
+        bottom=1,
+        left=1,
+        right=1,
+        borderType=cv2.BORDER_REFLECT_101
+    )
+
+    for i in range(tmp.shape[0]):
+        for j in range(tmp.shape[1]):
+
+            sobel = 0
+
+            for u in range(3):
+                for v in range(3):
+                    sobel += img[i+u,j+v] * kernel[2-u,2-v]
+
+            tmp[i,j] = int(np.round(sobel))
+
+    #Gefilternes Bild uebertragen
+    img = tmp
     return 0
 
 
 # Extra: create an integral image of the given image
 def createIntegralImage(img):
+    tmp = img
+
+    tmp = np.cumsum(tmp)
+
+    img=tmp
+
     return 0
 
 # Extra: apply the moving average filter by using an integral image
 def applyMovingAverageFilterWithIntegralImage(img, kSize):
+    tmp = img
+
+    borderSize = int(kSize/2)
+
+    img = cv2.copyMakeBorder(
+        img,
+        top=borderSize,
+        bottom=borderSize,
+        left=borderSize,
+        right=borderSize,
+        borderType=cv2.BORDER_REFLECT_101
+    )
+
+    integral, integralsq = cv2.integral2(img)
+
+    createIntegralImage(integral)
+
+    total_pixel = np.square(kSize*2+1)
+
+    for i in range(tmp.shape[0]):
+        for j in range(tmp.shape[1]):
+            s1 = (int(integral[i+kSize,j+kSize])
+            +int(integral[i-kSize,j-kSize])
+            -int(integral[i+kSize,j-kSize])
+            -int(integral[i-kSize,j+kSize]))
+
+            s2 = (int(integralsq[i+kSize,j+kSize])
+            +int(integralsq[i-kSize,j-kSize])
+            -int(integralsq[i+kSize,j-kSize])
+            -int(integralsq[i-kSize,j+kSize]))
+
+            std = (s2 - (np.square(s1))/total_pixel)/total_pixel
+            
+            if(std < 0): 
+                std = 0
+
+            std = np.sqrt(std)
+
+            tmp[i,j] = int(std)
+
+    img = tmp 
+
     return 0
 
 # Extra:
 def applyMovingAverageFilterWithSeperatedKernels(img, kSize):
+    #temporaeres Bild zum Speicher der Ergebnisse des Filters
+    tmp = img
+
+    borderSize = int(kSize/2)
+
+    #Bild erweitern, damit Groesse Eingang = Groese Ausgang
+    img = cv2.copyMakeBorder(
+        img,
+        top=borderSize,
+        bottom=borderSize,
+        left=borderSize,
+        right=borderSize,
+        borderType=cv2.BORDER_REFLECT_101
+    )
+
+    for i in range(tmp.shape[0]):
+        for j in range(tmp.shape[1]):
+
+            average_x = 0
+            average_y = 0
+
+            for u in range(kSize):
+                average_x += img[i,j+u]
+
+            for u in range(kSize):
+                average_y += img[i+u,j]
+
+            tmp[i,j] = int((average_x + average_y)/(2*kSize))
+
+    #Gefilternes Bild uebertragen
+
+    img = tmp
     return 0
--- Bildbearbeitung/Excercise3/main.py
+++ Bildbearbeitung/Excercise3/main.py
@@ -1,16 +1,128 @@
 import cv2
 import ImageFiltering as IF
 import numpy as np
-
-
-
+from datetime import datetime
+from matplotlib import pyplot as plt
 
 
 if __name__ == '__main__':
     # load image and apply filters
     # choose some sort of border handling that keeps the size of the processed image
 
+    #landen des Bildes und umwandeln in schwarz/weiss
+    img = cv2.imread("./SampleData/lena.png",1)
+    imgBW = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
+    #Schwarz/Weiss Bild zeigen und speichern
+    cv2.imshow("Schwarz/Weiss", imgBW)
+    cv2.imwrite("./Output/BlackWhiteImage.jpg", imgBW)
 
+    #Moving Average Bild Erstellen
+    imgMovingAverage = imgBW.copy()
+    IF.applyMovingAverageFilter(imgMovingAverage, 5)
 
+    #Moving Average Bild zeigen und speichern
+    cv2.imshow("Moving Average", imgMovingAverage)
+    cv2.imwrite("./Output/MovingAverageImage.jpg", imgMovingAverage)
 
+    #Guass Bild Erstellen
+    imgGauss = imgBW.copy()
+    IF.applyGaussFilter(imgGauss, 5, 1)
+
+    #Gauss Bild zeigen und speichern
+    cv2.imshow("Gauss", imgMovingAverage)
+    cv2.imwrite("./Output/GaussImage.jpg", imgGauss)
+
+    #Sobel Kernel erstellen
+    xKernel = IF.createSobelXKernel()
+    yKernel = IF.createSobelYKernel()
+
+    #Sobel Bilder Erstellung
+    imgXSobel = imgBW.copy()
+    imgYSobel = imgBW.copy()
+    IF.applyKernelInSpatialDomain(imgXSobel, xKernel)
+    IF.applyKernelInSpatialDomain(imgYSobel, yKernel)
+
+    #Sobel Bilder zeigen und speichern
+    cv2.imshow("XSobel", imgXSobel)
+    cv2.imwrite("./Output/XSobelImage.jpg", imgXSobel)
+
+    cv2.imshow("YSobel", imgYSobel)
+    cv2.imwrite("./Output/YSobelImage.jpg", imgYSobel)
+
+    #Median Bild Erstellung
+    imgMedian = imgBW.copy()
+    IF.applyMedianFilter(imgMedian, 5)
+
+    #Median Bild zeigen und speichern
+    cv2.imshow("Median", imgMedian)
+    cv2.imwrite("./Output/MedianImage.jpg", imgMedian)
+
+    #Moving Average Seperate Kernel Bild Erstellung
+    imgMASeperatedKernel = imgBW.copy()
+    IF.applyMovingAverageFilterWithSeperatedKernels(imgMASeperatedKernel, 5)
+
+    #Moving Average Seperate Kernel zeigen und speichern
+    cv2.imshow("Moving Average Seperated Kernel", imgMASeperatedKernel)
+    cv2.imwrite("./Output/MovingAverageSeperatedKernelImage.jpg", imgMASeperatedKernel)
+    
+    #Moving Average Integral Bild Erstellung
+    imgMAIntegral = imgBW.copy()
+    IF.applyMovingAverageFilterWithIntegralImage(imgMAIntegral, 5)
+
+    #Moving Average Seperate Kernel zeigen und speichern
+    cv2.imshow("Moving Average Integral", imgMAIntegral)
+    cv2.imwrite("./Output/MovingAverageIntegralImage.jpg", imgMAIntegral)
+
+    #Zeitmessen 
+    x_min = 3
+    x_max = 7
+
+    x = np.arange(x_min,x_max,2)
+    y_MA_Filter = np.zeros(x.shape)
+    y_MA_Seperate = np.zeros(x.shape)
+    y_MA_Intergral = np.zeros(x.shape)
+
+    imgTime = imgBW.copy()
+
+    for i in range(x.shape[0]):
+        kSize = x[i]
+
+        before = datetime.now()
+        IF.applyMovingAverageFilter(imgTime, kSize)
+        after = datetime.now()
+        td = after - before
+        y_MA_Filter[i] = td.total_seconds()
+
+        before = datetime.now()
+        IF.applyMovingAverageFilterWithSeperatedKernels(imgTime, kSize)
+        after = datetime.now()
+        td = after - before
+        y_MA_Seperate[i] = td.total_seconds()
+
+        before = datetime.now()
+        IF.applyMovingAverageFilterWithIntegralImage(imgTime, kSize)
+        after = datetime.now()
+        td = after - before
+        y_MA_Intergral[i] = td.total_seconds()
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot()
+
+    fig.suptitle('Zeitmessung bei verschiedenen Implementionen von Moving Average', fontsize=14, fontweight='bold')
+
+    ax.set_xlabel("Kernel Größe")
+    ax.set_ylabel("Dauer in Sekunden")
+
+    filter_plot = ax.plot(x,y_MA_Filter, label='Filter')
+    seperate_plot = ax.plot(x,y_MA_Seperate, label='Seperate Kernel')
+    intergral_plot = ax.plot(x,y_MA_Intergral, label='Integral')
+
+    ax.legend()
+
+    plt.show()
+
+    plt.savefig('./Output/Zeitmessen.jpg')
+
+    cv2.waitKey(0)
